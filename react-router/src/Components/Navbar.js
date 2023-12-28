119b7e0429e7ae62af2a5ad6eb0d99b4b44c55a1
femmytedrey <femmytedrey@gmail.com>
2023-12-28 23:54:57 +0100

    adding style to link within react

--- react-router/src/Components/Navbar.js
+++ react-router/src/Components/Navbar.js
@@ -2,10 +2,17 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
+  const NavLinkStyle = ({isActive}) => {
+    return {
+      fontWeight: isActive ? 'bold' : 'regular',
+      color: isActive ? 'rgb(135, 255, 255)' : 'teal'
+
+    }
+  }
   return (
     <nav>
-        <NavLink to = '/'>Home</NavLink>
-        <NavLink to = 'about'>About</NavLink>
+        <NavLink style={NavLinkStyle} to = '/'>Home</NavLink>
+        <NavLink style={NavLinkStyle} to = 'about'>About</NavLink>
     </nav>
   )
 }
