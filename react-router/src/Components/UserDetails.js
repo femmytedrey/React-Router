37fa1b3471c9ddbb8ef963bc14ab5f29958bf808
femmytedrey <femmytedrey@gmail.com>
2023-12-31 07:17:06 +0100

    working

--- react-router/src/Components/UserDetails.js
+++ react-router/src/Components/UserDetails.js
@@ -4,7 +4,6 @@ import { useParams } from 'react-router-dom'
 export const UserDetails = () => {
     const { userId } = useParams()
 
-    
   return (
     <div>Details about user {userId}</div>
   )
