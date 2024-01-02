ab4bc24dbddcd3a763bc6096863978854039632a
femmytedrey <femmytedrey@gmail.com>
2024-01-03 00:05:48 +0100

    authentication and page privacy with router

--- react-router/src/Components/Login.js
+++ react-router/src/Components/Login.js
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import { useAuth } from './auth';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+
 
 export const Login = () => {
     const [user, setUser] = useState('');
     const auth = useAuth()
     const navigate = useNavigate()
+    const location = useLocation()
+
+    const redirectPath = location.state?.path || '/'
 
     const handleLogin = () => {
         auth.login(user)
-        navigate('/')
+        navigate(redirectPath, {replace: true})
     }
   return (
     <div>
--- react-router/src/Components/RequireAuth.js
+++ react-router/src/Components/RequireAuth.js
@@ -1,11 +1,12 @@
-import { Navigate } from 'react-router-dom'
+import { Navigate, useLocation } from 'react-router-dom'
 import React from 'react'
 import { useAuth } from './auth'
 
 export const RequireAuth = ({children}) => {
+    const location = useLocation()
     const Auth = useAuth()
     if(!Auth.user){
-        return <Navigate to = '/login' />
+        return <Navigate to = '/login' state={{ path: location.pathname }} />
     }
   return children
 }
